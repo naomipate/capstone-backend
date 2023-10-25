e04035f914cca07e3ef7f3e50efc57b5df011428
Naomi-Pate <patenaomi99@gmail.com>
2023-10-25 11:06:11 -0400

    updated .env

--- controllers/example.controller.js
+++ controllers/example.controller.js
@@ -1,34 +1,34 @@
 // DEPENDENCIES
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-let exampleModel = require('../models/example.model');
+let exampleModel = require("../models/example.model");
 
 // Make sure your model is working
 console.log(exampleModel);
 
 // GET
-router.get('/:id', (req, res) => {
-    // Controller logic to get a single item
+router.get("/:id", (req, res) => {
+  // Controller logic to get a single item
 });
 
-router.get('/', (req, res) => {
-    // Controller logic to get all items
-    res.status(200).send(exampleModel);
+router.get("/", (req, res) => {
+  // Controller logic to get all items
+  res.status(200).send(exampleModel);
 });
 
 // POST
-router.post('/create', (req, res) => {
-    // Controller logic to create a new item
+router.post("/", (req, res) => {
+  // Controller logic to create a new item
 });
 
 // EDIT
-router.put('/edit/:id', (req, res) => {
-    // Controller logic to edit an item
+router.put("/:id", (req, res) => {
+  // Controller logic to edit an item
 });
 
 // DELETE
-router.delete('/delete/:id', (req, res) => {
-    // Controller logic to delete an item
+router.delete("/:id", (req, res) => {
+  // Controller logic to delete an item
 });
 
 // EXPORT
