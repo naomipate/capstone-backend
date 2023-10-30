0a312c1e5ab42b1597110bd09c7104f59b429af8
Naomi-Pate <patenaomi99@gmail.com>
2023-10-30 10:32:13 -0400

    updated backend with schema.sql

---
+++ db/dbConfig.js
@@ -0,0 +1,19 @@
+const pgp = require("pg-promise")();
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+};
+const db = pgp(cn);
+db.connect()
+  .then((obj) => {
+    const serverVersion = obj.client.serverVersion;
+    console.log("postgres connection established");
+    obj.done();
+  })
+  .catch((error) => {
+    console.log("ERROR", error.message || error);
+  });
+module.exports = db;
