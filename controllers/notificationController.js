02baa014efdeaceced05bfd6834ce31abad2c184
AkiraBrown <akira.watson.brown@gmail.com>
2023-12-10 16:51:13 -0500

    AB stash 0451

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -33,6 +33,7 @@ router.get("/:id", async (req, res) => {
 });
 // Creates a new Notification
 router.post("/new-notification", async (req, res) => {
+  console.log(req.body);
   try {
     const addedNoti = await addNotification(req.body);
     res.status(200).json(addedNoti);
