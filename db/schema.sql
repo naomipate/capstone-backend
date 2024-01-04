78dc2634e73eeadf260897d197a512a0186584f6
Naomi-Pate <patenaomi99@gmail.com>
2024-01-04 01:17:16 -0500

    Added user_picture URLs

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ CREATE DATABASE giftune_db;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
+    user_picture TEXT NOT NULL,
     user_name VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -14,29 +15,29 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users(user_name, first_name, last_name, dob, email) VALUES
-('Akira', 'Akira', 'Brown', '1998-12-21', 'akira@email.com'),
-('Chantal', 'Chantal', 'Gomez', '1999-05-13', 'chantal@email.com'),
-('Onyx', 'Onyx', 'McQueen', '1997-01-06', 'onyx@email.com'),
-('Naomi', 'Naomi', 'Pate', '1994-09-10', 'naomi@email.com'),
-('Mussie', 'Mussie', 'Russom','1989-02-27', 'mussie@email.com'),
-('Casey', 'Casey', 'Smith', '1999-10-10', 'casey@email.com'),
-('Jeff', 'Jeff', 'Helman', '1998-03-12', 'jeff@email.com'),
-('Emily', 'Emily', 'Johnson', '1995-03-15', 'Emily@email.com'),
-('Benjamin', 'Benjamin', 'Martinez', '1992-11-05', 'Benjamin@email.com'),
-('Sophia', 'Sophia', 'Lee', '1998-04-20', 'Sophia@email.com'),
-('Jackson', 'Jackson', 'Thompson', '1993-09-08', 'Jackson@email.com'),
-('Olivia', 'Olivia', 'Rodriguez', '1996-05-10', 'Olivia@email.com'),
-('Ethan', 'Ethan', 'Adams', '2000-07-03', 'Ethan@email.com'),
-('Ava', 'Ava', 'Wright', '1991-02-18', 'Ava@email.com'),
-('Liam', 'Liam', 'Campbell', '1994-10-30', 'Liam@email.com'),
-('Charlotte', 'Charlotte', 'White', '1997-06-25', 'Charlotte@email.com'),
-('Mason', 'Mason', 'Rivera', '2001-12-12', 'Mason@email.com'),
-('Amelia', 'Amelia', 'Cooper', '1999-08-07', 'Amelia@email.com'),
-('Noah', 'Noah', 'Hughes', '1990-01-22', 'Noah@email.com'),
-('Isabella', 'Isabella', 'Patel', '1993-11-14', 'Isabella@email.com'),
-('Lucas', 'Lucas', 'Mitchell', '1992-03-05', 'Lucas@email.com'),
-('Mia', 'Mia', 'Carter', '1995-09-28', 'Mia@email.com');
+INSERT INTO users(user_picture, user_name, first_name, last_name, dob, email) VALUES
+('https://media.licdn.com/dms/image/D4E35AQE39nRIs8i93w/profile-framedphoto-shrink_800_800/0/1680028937815?e=1704952800&v=beta&t=kXWCG52Bl1aF_YhQlg6qV0Lyo67I0MVOlXO1YdQFyM4', 'Akira', 'Akira', 'Brown', '1998-12-21', 'akira@email.com'),
+('https://media.licdn.com/dms/image/D4E03AQEsnPHbNKvuqg/profile-displayphoto-shrink_800_800/0/1666462641490?e=1709769600&v=beta&t=a3Sa5OXl7l17IuGlYnJFmEWWyl1p37ClNESQqrSV61A', 'Chantal', 'Chantal', 'Gomez', '1999-05-13', 'chantal@email.com'),
+('https://media.licdn.com/dms/image/D4E03AQFScV89mnk4Aw/profile-displayphoto-shrink_800_800/0/1684863900954?e=1709769600&v=beta&t=8cJ9MdLnlv7RU0MPAFzqMsfG77JnHJQQb1dqIsZrweE', 'Onyx', 'Onyx', 'McQueen', '1997-01-06', 'onyx@email.com'),
+('https://media.licdn.com/dms/image/D4E35AQHRK06z0YZmVw/profile-framedphoto-shrink_800_800/0/1680637669846?e=1704952800&v=beta&t=ppq0bIeV-nX3CsBjrE-iEuKNpk7R_n94Ku2t9t0M-yc', 'Naomi', 'Naomi', 'Pate', '1994-09-10', 'naomi@email.com'),
+('https://media.licdn.com/dms/image/D4D03AQHKxVlTxMLP3A/profile-displayphoto-shrink_800_800/0/1667417011554?e=1709769600&v=beta&t=5hSB4KGm9jgWyhs8Y11GGrRPd6-qzmPg98tGnfv9pfY', 'Mussie', 'Mussie', 'Russom','1989-02-27', 'mussie@email.com'),
+('https://media.licdn.com/dms/image/C4D03AQEsLqgjUlaYWw/profile-displayphoto-shrink_800_800/0/1611197045175?e=2147483647&v=beta&t=x79y0VBQaNGByF3-U_yzX-jovZx9mqCk8cohRV4hKMA', 'Casey', 'Casey', 'Smith', '1999-10-10', 'casey@email.com'),
+('https://media.licdn.com/dms/image/C5603AQHBttC7oWHiBg/profile-displayphoto-shrink_800_800/0/1571845515068?e=2147483647&v=beta&t=CZQJZZU1uLMyQ3dvkPFdK2DUl3VqXGK1XcVLu0RqXs0', 'Jeff', 'Jeff', 'Helman', '1998-03-12', 'jeff@email.com'),
+('https://media.licdn.com/dms/image/D5603AQEMKR2brT3tRg/profile-displayphoto-shrink_800_800/0/1680093433513?e=2147483647&v=beta&t=lPygH-Du9CHl3AY5TpdsVEYyWel509Mu5bijOaYA_sI', 'Emily', 'Emily', 'Johnson', '1995-03-15', 'emily@email.com'),
+('https://media.licdn.com/dms/image/C4E03AQFE1Le99AZKlA/profile-displayphoto-shrink_800_800/0/1634397225450?e=2147483647&v=beta&t=tReNwAGrMpYpK3cgaIaFwbLGLMBuwb6OZu4oHlIycv8', 'Benjamin', 'Benjamin', 'Martinez', '1992-11-05', 'benjamin@email.com'),
+('https://media.licdn.com/dms/image/C5603AQE7M_M29W5oyw/profile-displayphoto-shrink_800_800/0/1638937317754?e=2147483647&v=beta&t=99-1HgWhb6iSoaB-lGV80NbHJIos7GxzJUkWxXFgtMw', 'Sophia', 'Sophia', 'Lee', '1998-04-20', 'sophia@email.com'),
+('https://media.licdn.com/dms/image/C4E03AQEMo1nP8kZ0rg/profile-displayphoto-shrink_800_800/0/1631128442758?e=2147483647&v=beta&t=Jx9Svamf0RXe-PNbNvU5UZQgv2PKMmgyQWMEtGO6ZUE', 'Jackson', 'Jackson', 'Thompson', '1993-09-08', 'jackson@email.com'),
+('https://media.licdn.com/dms/image/C4D03AQGDz-SClAvXyA/profile-displayphoto-shrink_400_400/0/1516239195769?e=2147483647&v=beta&t=IQYSEwUPXzINuvZTiAFtm5-fNYVgujWBsiqCRxyACRk', 'Olivia', 'Olivia', 'Rodriguez', '1996-05-10', 'olivia@email.com'),
+('https://media.licdn.com/dms/image/C5603AQGrdVAxieo8jA/profile-displayphoto-shrink_800_800/0/1663353983435?e=2147483647&v=beta&t=riYsqfqw2RTviya5SRzyNdTX9SwKVh0FR8s2JLzDnEw', 'Ethan', 'Ethan', 'Adams', '2000-07-03', 'ethan@email.com'),
+('https://media.licdn.com/dms/image/C5603AQFDP9dmU0AyNg/profile-displayphoto-shrink_800_800/0/1621292749467?e=2147483647&v=beta&t=h7lfjI0c5x4PuU3RVpIjtygeWrRqVz6R4hgWuRBGDFA', 'Ava', 'Ava', 'Wright', '1991-02-18', 'ava@email.com'),
+('https://media.licdn.com/dms/image/C4E03AQF0CrUJJJ1vzg/profile-displayphoto-shrink_800_800/0/1605277914373?e=2147483647&v=beta&t=7e1cB1V2j1Y4MLOPRxCWc8Wli2ZRvXZlJ8mesqYLf5Q', 'Liam', 'Liam', 'Campbell', '1994-10-30', 'liam@email.com'),
+('https://media.licdn.com/dms/image/D5603AQFeL3BFWSYA5g/profile-displayphoto-shrink_800_800/0/1691552343925?e=2147483647&v=beta&t=DUwhy7PgKqJ_UMOUUhxZbt9iCkY-vsE8TRFTsah1kPg', 'Charlotte', 'Charlotte', 'White', '1997-06-25', 'charlotte@email.com'),
+('https://media.licdn.com/dms/image/D4E03AQEP8Lou5s9gBA/profile-displayphoto-shrink_400_400/0/1697847273774?e=2147483647&v=beta&t=de3pSHJrmwv3B4N77jxo5oslGLKMECiPaC5Lee_D9fY', 'Mason', 'Mason', 'Rivera', '2001-12-12', 'mason@email.com'),
+('https://media.licdn.com/dms/image/D5603AQFXLwaDRhFq-A/profile-displayphoto-shrink_800_800/0/1683683415176?e=2147483647&v=beta&t=8zjlcppkzIM0GJK4wuxicyyaMGFukQRkdZtm3YQHuG4', 'Amelia', 'Amelia', 'Cooper', '1999-08-07', 'amelia@email.com'),
+('https://media.licdn.com/dms/image/C4D03AQFoLU3Gcbkwig/profile-displayphoto-shrink_400_400/0/1652389971227?e=2147483647&v=beta&t=cU4ivopN78IvcLH_ihYWFbI0DR0f2MvcdJQ1kvLJjpM', 'Noah', 'Noah', 'Hughes', '1990-01-22', 'noah@email.com'),
+('https://media.licdn.com/dms/image/C4E03AQFeq-dFBcZylw/profile-displayphoto-shrink_800_800/0/1614800221757?e=2147483647&v=beta&t=G2XIj2JuNDHf1ANrdkKXjjUM1rHIAaLpk9Tk_YYmHF0', 'Isabella', 'Isabella', 'Patel', '1993-11-14', 'isabella@email.com'),
+('https://media.licdn.com/dms/image/D4E03AQEkHBFT4uXbYw/profile-displayphoto-shrink_800_800/0/1697050247881?e=2147483647&v=beta&t=qLHo-mKfBLyJmhNO8V9kqutB-b0Tj1sINNVK2BzLAWw', 'Lucas', 'Lucas', 'Mitchell', '1992-03-05', 'lucas@email.com'),
+('https://media.licdn.com/dms/image/D5603AQH8Pbins7S52g/profile-displayphoto-shrink_800_800/0/1693065944348?e=2147483647&v=beta&t=upwn_2wrvKn4BwMb80REQXvJ0jN7t0XPla13ccPtUhs', 'Mia', 'Mia', 'Carter', '1995-09-28', 'mia@email.com');
 
 
 DROP TABLE IF EXISTS friends_list;
