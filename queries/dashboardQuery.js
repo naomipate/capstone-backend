989e8f660e33f0377945a3e02c853ce861107a46
Naomi-Pate <patenaomi99@gmail.com>
2024-01-05 12:11:04 -0500

    fixed dashboard query

--- queries/dashboardQuery.js
+++ queries/dashboardQuery.js
@@ -7,47 +7,6 @@ const getUserProfile = async (id) => {
   return user;
 };
 
-const getFriendsAndTheirWishlists = async (id) => {
-  // Friends
-  const requests = await db.any(
-    "SELECT * FROM friends_list WHERE user_id=$1 OR friends_id=$1",
-    id
-  );
-  // comparing to confirm friendship
-  let usersRequest = requests
-    .filter((userRequest) => userRequest.user_id === Number(id))
-    .map((request) => request.friends_id);
-
-  let friendsRequest = requests
-    .filter((friendRequest) => friendRequest.friends_id === Number(id))
-    .map((request) => request.user_id);
-
-  // friendship confirmed and pushed into array for Friends list
-  let connectionIds = [];
-  for (let requestId of usersRequest) {
-    if (friendsRequest.includes(requestId)) {
-      connectionIds.push(requestId);
-    }
-  }
-
-  // Ordering friends list by DOB
-  let userConnections = await db.any(
-    "SELECT * FROM users WHERE id in ($1:csv) ORDER BY dob ASC",
-    [connectionIds]
-  );
-
-  // Grabbing friends' wishlists by user_id
-  for (let connection of userConnections) {
-    let wishlist = await db.any(
-      "SELECT * FROM wishlist WHERE user_id=$1",
-      connection.id
-    );
-    connection.wishlist = wishlist;
-  }
-
-  return userConnections;
-};
-
 const getAllFriendsFromUser = async (id) => {
   try {
     const allFriendsFromUser = await db.any(
@@ -115,7 +74,6 @@ const addFriendEntryFriendsList = async (user_id, friend_id) => {
 module.exports = {
   getUserProfile,
   getAllFriendsFromUser,
-  getFriendsAndTheirWishlists,
   getWishlistById,
   deleteFriendEntryFriendsList,
   updateItemBoughtByItemId,
